b1519be6e19ca7f9928bab384263223dc4a27634
tuhtdev <85549283+tuhtdev@users.noreply.github.com>
2022-12-23 10:35:13 +0700

    ok

--- src/main/java/com/lendbiz/p2p/api/service/impl/VNPTServiceImpl.java
+++ src/main/java/com/lendbiz/p2p/api/service/impl/VNPTServiceImpl.java
@@ -6,12 +6,16 @@ import com.lendbiz.p2p.api.constants.Constants;
 import com.lendbiz.p2p.api.constants.ErrorCode;
 import com.lendbiz.p2p.api.entity.vnpt.BgEkycEntity;
 import com.lendbiz.p2p.api.exception.BusinessException;
+import com.lendbiz.p2p.api.producer.ProducerMessage;
 import com.lendbiz.p2p.api.repository.BgEkycRepository;
 import com.lendbiz.p2p.api.repository.CfMastRepository;
 import com.lendbiz.p2p.api.response.BaseResponse;
 import com.lendbiz.p2p.api.service.VNPTService;
 import com.lendbiz.p2p.api.service.base.BaseService;
 import lombok.SneakyThrows;
+
+import org.apache.commons.codec.binary.Base64;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.io.ByteArrayResource;
@@ -39,6 +43,9 @@ public class VNPTServiceImpl extends BaseResponse<VNPTService> implements VNPTSe
     @Qualifier("cfMastRepository")
     @Autowired
     CfMastRepository cfMastRepo;
+
+    @Autowired
+    private ProducerMessage producerMessage;
 
     public String uploadImage(MultipartFile image, String title, String description) {
         // Phí 0 vnd
@@ -82,6 +89,9 @@ public class VNPTServiceImpl extends BaseResponse<VNPTService> implements VNPTSe
         String hashImgFrontId = this.uploadImage(imgFrontId, "imgFrontId", "imgFrontId");
         String hashImgBackId = this.uploadImage(imgBackId, "imgBackId", "imgBackId");
         logger.info("============= Start eKYC vertifyIdentity (VNPT) =============");
+
+        saveFileKafka(imgFrontId, mobile, 0);
+        saveFileKafka(imgBackId, mobile, 1);
 
         // Phí 800 vnd
         BgEkycEntity bgEkyc = this.bgEkycRepository.findByMobileSms(mobile);
@@ -223,6 +233,8 @@ public class VNPTServiceImpl extends BaseResponse<VNPTService> implements VNPTSe
         String hashImgSelfie = this.uploadImage(imgSelfie, "imgSelfie", "imgSelfie");
 
         logger.info("============= Start eKYC vertifySelfie (VNPT) =============");
+        saveFileKafka(imgSelfie, mobile, 2);
+
         // Phí 800 vnd
         BgEkycEntity bgEkyc = this.bgEkycRepository.findByMobileSms(mobile);
         if(bgEkyc == null) bgEkyc = new BgEkycEntity();
@@ -278,5 +290,23 @@ public class VNPTServiceImpl extends BaseResponse<VNPTService> implements VNPTSe
         bgEkyc.setCompareSuccess("YES");
         this.bgEkycRepository.save(bgEkyc);
         return response(toResult(Constants.SUCCESS, Constants.MESSAGE_SUCCESS, root.get("object")));
+    }
+
+    public void saveFileKafka(MultipartFile file, String mobile, int type) {
+        try {
+            byte[] fileContent = Base64.encodeBase64(file.getBytes());
+            String data = new String(fileContent, "UTF-8");
+
+            Map<String, Object> map = new HashMap<>();
+            map.put("mobile", mobile);
+            map.put("file", data);
+            map.put("fileName", type + "_" + file.getOriginalFilename());
+
+            JSONObject jsonObjectLogs = new JSONObject(map);
+            producerMessage.sendSaveIdCard(jsonObjectLogs.toString());
+
+        } catch (Exception e) {
+            logger.info(e.getMessage());
+        }
     }
 }
