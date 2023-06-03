31981f078b5067aa9488a299e3d5c1568bfa6ab1
tuhtdev <85549283+tuhtdev@users.noreply.github.com>
2023-06-03 10:35:01 +0700

    Update GameConfigEntity.java

--- src/main/java/com/lendbiz/p2p/api/entity/GameConfigEntity.java
+++ src/main/java/com/lendbiz/p2p/api/entity/GameConfigEntity.java
@@ -58,7 +58,7 @@ public class GameConfigEntity {
         private String toDate;
 
         @Column(name = "MAXPRIZE")
-        private Integer maxPrize;
+        private BigInteger maxPrize;
 
         @Column(name = "CONFIGID")
         private Integer configId;
